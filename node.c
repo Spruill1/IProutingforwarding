e20e98479bded9df8d6490066547fd91fabf5b27
Spruill1 <david.spruill@duke.edu>
2015-03-02 12:44:13 -0500

    added the rest of the data structure, now we need to add functions to encapsulate the actual socket commands...   look into fd/socket to simultaneously use multiple sockets

--- node.c
+++ node.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 #include "ipsum.h"
-#include "/usr/include/netinet/ip.h"
+#include <netinet/ip.h>
 
-#include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <arpa/inet.h>
 
+#include <string.h>
 #include <fstream>
 #include <sstream>
 #include <vector>
@@ -16,90 +16,127 @@
 
 using namespace std;
 
-#define IP_LENGTH = 16
-#define TTL_MAX = 16
-#define MTU_MAX = 1400
-#define IN_BUFFER_SIZE = (1024 * 64)
-#define UPDATE_TIMER = 5000 //(5000ms = 5seconds)
-#define EXPIRE_TIMER = 12000
-#define ROUTING_ENTRIES_MAX = 64
-
+#define IP_LENGTH 16
+#define TTL_MAX 16
+#define MTU_MAX 1400
+#define IN_BUFFER_SIZE (1024 * 64)
+#define UPDATE_TIMER 5000 //(5000ms = 5seconds)
+#define EXPIRE_TIMER 12000
+#define ROUTING_ENTRIES_MAX 64
+#define RIP_DATA 200
 
 typedef struct node{
-	string myAddr;
-	int myPort;
-
-	void print(){printf("node:\t%s:%d\n",myAddr.c_str(),myPort);}
+	char IP_me[IP_LENGTH];
+	int port_me;
+	
+	node(){port_me = -1; memset(&IP_me[0], 0, IP_LENGTH);}
+	void print(){printf("node:\t%s:%d\n",IP_me,port_me);}
 } node;
 
 typedef struct net_interface{
-	char IPofRemote;
-	int PortofRemote;
-	string VIPofMyInterface;
-	string VIPofRemoteInterface;
+	int id;
 
+	char IP_remote[IP_LENGTH];
+	uint16_t port_remote;
+	char vip_me[IP_LENGTH];
+	char vip_remote[IP_LENGTH];
 
 	int sock;	
+	bool up;
 
+	net_interface(){memset(&IP_remote[0], 0, IP_LENGTH);
+			port_remote = -1;
+			memset(&vip_me[0], 0, IP_LENGTH);
+			memset(&vip_remote[0], 0, IP_LENGTH);}
 	void print(){
 		printf("net_interface:\n\t%s:%d\n\t%s\n\t%s\n",
-			IPofRemote.c_str(),PortofRemote,
-			VIPofMyInterface.c_str(),VIPofRemoteInterface.c_str());
+			IP_remote,port_remote,vip_me,vip_remote);
+	}
+	void initSocket(){
+		if (sock = socket(AF_INET, SOCK_DGRAM/*use UDP*/, 0) < 0 ){
+			perror("Create socket failed:");
+			exit(1);
+		}
 	}
 
 } net_interface;
 
-int readFile(char* path, node *myNode, vector<net_interface> * myInterfaces);
+typedef struct forwarding_table_entry {
+	char dest[IP_LENGTH];
+	uint16_t cost;
+	int int_id;
+	
+	forwarding_table_entry() {memset(&dest[0], 0, IP_LENGTH);
+				  cost=TTL_MAX;
+				  int_id = -1;}
+} forwarding_table_entry;
+
+typedef struct forwarding_table {
+	uint16_t num_entries;
+	forwarding_table_entry entries[ROUTING_ENTRIES_MAX];
+} forwarding_table;
+
+typedef struct RIP {
+	uint16_t command;
+	uint16_t num_entries;
+	struct {
+		uint32_t cost;
+		uint32_t address;
+	} entries[ROUTING_ENTRIES_MAX];
+} RIP;
+
+node Node; //global for this node's information
+forwarding_table forwardingTable; 
+
+int readFile(char* path, node *Node, vector<net_interface> * myInterfaces);
 
 int main(int argv, char* argc[]){
 
 	//if there is no arguments, then exit
 	if (argv < 2) {
 			perror("No input file:");
-			return -1;
+			exit(1);
 	}
+	
+		
 
-	node myNode;
 	vector<net_interface> myInterfaces;
-	if(int err = readFile(argc[1],&myNode,&myInterfaces) < 0) {return err;} //get the file's information
+	if(int err = readFile(argc[1],&Node,&myInterfaces) < 0) {return err;} //get the file's information
 
 
 }
 
-int readFile(char* path, node *myNode, vector<net_interface> * myInterfaces) {
+int readFile(char* path, node *Node, vector<net_interface> * myInterfaces) {
 	ifstream fin(path);
 
 	string myInfo;
 	getline(fin,myInfo);
 	
 	//get the IP & Port for this node
-	myNode->myAddr = myInfo.substr(0,myInfo.find(":"));
-	myNode->myPort = atoi(myInfo.substr(myInfo.find(":")+1,myInfo.npos).c_str());
+	myInfo.substr(0,myInfo.find(":")).copy(Node->IP_me,IP_LENGTH,0);
+	Node->port_me = atoi(myInfo.substr(myInfo.find(":")+1,myInfo.npos).c_str());
 
-	myNode->print();
+	Node->print();
 
 	//get the information for the interfaces
 	while(!fin.eof()){
 		myInfo.erase(0,myInfo.length());
 		getline(fin,myInfo);
 		net_interface myInt;
-		myInt.IPofRemote = myInfo.substr(0,myInfo.find(":"));
+		myInfo.substr(0,myInfo.find(":")).copy(myInt.IP_remote,IP_LENGTH,0);
 			myInfo.erase(0,myInfo.find(":")+1);
-		myInt.PortofRemote = atoi(myInfo.substr(0,myInfo.find(" ")).c_str());
+		myInt.port_remote = atoi(myInfo.substr(0,myInfo.find(" ")).c_str());
 			myInfo.erase(0,myInfo.find(" ")+1);
-		myInt.VIPofMyInterface = myInfo.substr(0,myInfo.find(" "));
+		myInfo.substr(0,myInfo.find(" ")).copy(myInt.vip_me,IP_LENGTH,0);
 			myInfo.erase(0,myInfo.find(" ")+1);
-		myInt.VIPofRemoteInterface = myInfo;
+		myInfo.copy(myInt.vip_remote,IP_LENGTH,0);
 		
-		if(myInt.IPofRemote.length()>0){
+		if(strlen(myInt.IP_remote)>0){
 		myInterfaces->push_back(myInt);}
 	}
 	for(vector<net_interface>::iterator iter = myInterfaces->begin(); iter != myInterfaces->end(); ++iter)
 	{ 
 		iter->print(); 
-		if (iter->sock = socket(AF_INET, SOCK_DGRAM/*use UDP*/, 0) < 0 ){
-			perror("Create socket failed:");
-			return -1;
-		}
+		iter->initSocket();
 	}
 }
